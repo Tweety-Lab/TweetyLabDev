9b8f79ab7f29b4f4171ce8ff999dc3c532e3fbf5
Tweety <allie.g@tweety-lab.dev>
2026-06-28 00:52:19 +1000

    Add article metadata.

---
+++ article-metadata.js
@@ -0,0 +1,1 @@
+// Metadata about all available articles
---
+++ portfolio-metadata.js
@@ -0,0 +1,27 @@
+// Metadata about all Portfolio projects
+export const projects = [
+    {
+        id: "source-csharp",
+        title: "Source C#",
+        thumbnail: "projects/source-csharp/thumb.png",
+        description: "Modified version of Valve's 2013 Source SDK that adds C# Scripting support.",
+    },
+    {
+        id: "tweety-lang",
+        title: "Tweety Lang",
+        thumbnail: "projects/tweety-lang/thumb.png",
+        description: "Native programming language designed around the philosophy of tools that Just Work.",
+    },
+    {
+        id: "source-rewrite",
+        title: "Source Rewrite",
+        thumbnail: "projects/source-rewrite/thumb.png",
+        description: "Semi-rewrite of Valve's Source Engine in C#.",
+    },
+    {
+        id: "netleaf",
+        title: "NetLeaf",
+        thumbnail: "projects/netleaf/thumb.png",
+        description: "C++ library that simplifies embedding C# into native applications.",
+    }
+];
